d67145acf82cf20b6acc4915895091bcec6bb438
Radhesh Sarma <50641461+Radhesh-Sarma@users.noreply.github.com>
2019-06-06 15:21:24 +0530

    email_sender.py

---
+++ email_sender.py
@@ -0,0 +1,137 @@
+from __future__ import print_function
+from googleapiclient.discovery import build
+from httplib2 import Http
+from oauth2client import file, client, tools
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email.mime.image import MIMEImage
+from email.mime.audio import MIMEAudio
+from email.encoders import encode_base64
+import base64
+import mimetypes
+import os
+
+def create_message(sender, to, subject, message_text):
+    """Create a message for an email.
+
+    Args:
+        sender: Email address of the sender.
+        to: Email address of the receiver.
+        subject: The subject of the email message.
+        message_text: The text of the email message.
+
+    Returns:
+        An object containing a base64url encoded email object.
+    """
+    message = MIMEText(message_text,'html')
+    message['to'] = to
+    message['from'] = sender
+    message['subject'] = subject
+    return {'raw': base64.urlsafe_b64encode(message.as_string())}
+
+def create_message_with_attachment(
+    sender, to, subject, message_text, file):
+    """Create a message for an email.
+
+      Args:
+        sender: Email address of the sender.
+        to: Email address of the receiver.
+        subject: The subject of the email message.
+        message_text: The text of the email message.
+        file: The path to the file to be attached.
+
+      Returns:
+        An object containing a base64url encoded email object.
+    """
+    message = MIMEMultipart()
+    message['to'] = to
+    message['from'] = sender
+    message['subject'] = subject
+
+    msg = MIMEText(message_text, 'html')
+    message.attach(msg)
+
+    content_type, encoding = mimetypes.guess_type(file)
+
+    if content_type is None or encoding is not None:
+        content_type = 'application/octet-stream'
+    main_type, sub_type = content_type.split('/', 1)
+    if main_type == 'text':
+        fp = open(file, 'rb')
+        msg = MIMEText(fp.read(), _subtype=sub_type)
+        fp.close()
+    elif main_type == 'image':
+        fp = open(file, 'rb')
+        msg = MIMEImage(fp.read(), _subtype=sub_type)
+        fp.close()
+    elif main_type == 'audio':
+        fp = open(file, 'rb')
+        msg = MIMEAudio(fp.read(), _subtype=sub_type)
+        fp.close()
+    else:
+        fp = open(file, 'rb')
+        msg = MIMEBase(main_type, sub_type)
+        msg.set_payload(fp.read())
+        fp.close()
+    
+    filename = os.path.basename(file)
+    msg.add_header('Content-Disposition', 'attachment', filename=filename)
+    encode_base64(msg)
+    message.attach(msg)
+
+
+
+    
+    return {'raw': base64.urlsafe_b64encode(message.as_string().encode())}
+
+
+def send_message(service, user_id, message):
+    """Send an email message.
+
+    Args:
+        service: Authorized Gmail API service instance.
+        user_id: User's email address. The special value "me"
+        can be used to indicate the authenticated user.
+        message: Message to be sent.
+
+    Returns:
+        Sent Message.
+    """
+    
+    message['raw'] = message['raw'].decode('utf-8')    
+    try:
+        message = (service.users().messages().send(userId=user_id, body=message).execute())
+        print('Message Id: %s' % message['id'])
+        return message
+    except :
+        print('An error occurred')
+
+
+
+
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = 'https://www.googleapis.com/auth/gmail.send'
+
+def send_mail(mail):
+    store = file.Storage('token.json')
+    creds = store.get()
+    if not creds or creds.invalid:
+        flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
+        creds = tools.run_flow(flow, store)
+    service = build('gmail', 'v1', http=creds.authorize(Http()))
+
+    sender = mail['sender']
+    reciever = mail['reciever']
+    subject = mail['subject']
+    msg_text = mail['body']
+    attach = mail['file']
+
+    #msg = create_message(sender, reciever, subject, msg_text)
+    if (attach != None) : 
+        msg = create_message_with_attachment(sender, reciever, subject, msg_text, attach) 
+        try :
+            send_message(service, "me", msg)
+        except :
+            print ('ERROR : ' + mail[reciever])
