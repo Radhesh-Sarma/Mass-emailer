8692498095571dec85ac485d09f70b90cb29c743
Radhesh Sarma <50641461+Radhesh-Sarma@users.noreply.github.com>
2019-06-09 16:54:29 +0530

    Bug Fix for audio,image files

--- email_sender.py
+++ email_sender.py
@@ -3,7 +3,9 @@ from googleapiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
 from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
+from email.mime.audio import MIMEAudio
 from email.mime.base import MIMEBase
 from email.encoders import encode_base64
 import base64
